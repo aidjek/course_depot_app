1710ae8b33596b8af9646fab514bef7c611a60ea
Aidjek <aidjek@gmail.com>
2012-12-04 22:53:15 +0400

    added a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(	:title=>'Capital',
+				:description=>%{
+					Love, prejudice and zombies},
+				:image_url=>'/images/capital.png',
+				:price=>99.99)
