3e96e6cf3a07646c370ca0263177426001122a59
Aidjek <aidjek@gmail.com>
2012-12-06 22:54:26 +0400

    Added validation products tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :price, :presence => true
-  validates :title, :uniqueness => true
+#  validates :title, :uniqueness => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => { :with => %r{\.(jpg|gif|png)$}i,
   									 :message => "Only jpg,png,gif required!"}
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validates presents of atttibutes" do |variable|
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  end
+  test "validates price" do
+  	product = Product.new(:title => "string", :description => "string", :image_url => "string")
+  	product.price = 1
+  	assert (product.price).invalid?
+  	product.price = -0.01
+  	assert (product.price).invalid?
+  	product.price = 0.0
+  	assert (product.price).invalid?
+  	product.price = 0.01
+  	assert (product.price).valid?
+  end
+  test "valid uniqueness" do
+  	product = Product.new( :description => "string", :image_url => "string", :price => 99 )
+  	product.title = products(:alice).title
+  	assert !product.save
+  end
 end
